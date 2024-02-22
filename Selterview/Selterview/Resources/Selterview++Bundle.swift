ac8f258ba692c8176e88ec4fb60d514e59f48455
woo0 <57060443+woo0dev@users.noreply.github.com>
2024-02-22 21:35:36 +0900

    feat/Main-Update api key bundle

--- Selterview/Selterview/Resources/Selterview++Bundle.swift
+++ Selterview/Selterview/Resources/Selterview++Bundle.swift
@@ -9,16 +9,9 @@ import Foundation
 
 extension Bundle {
 	var openAIAPIKey: String {
-		var fileString: String
-		if let file = self.path(forResource: "CI_Key", ofType: "plist") {
-			fileString = file
-		} else if let file = self.path(forResource: "API_Key", ofType: "plist") {
-			fileString = file
-		} else {
-			return ""
-		}
+		guard let file = self.path(forResource: "API_Key", ofType: "plist") else { return "" }
 		
-		guard let resource = NSDictionary(contentsOfFile: fileString) else { return "" }
+		guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
 		guard let key = resource["OpenAIAPIKey"] as? String else { fatalError("API_Key.plist에 OpenAIAPIKey를 등록해주세요.")}
 		return key
 	}
