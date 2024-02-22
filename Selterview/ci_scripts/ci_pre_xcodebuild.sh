5631a4dff1d3c5817bde0cdb4b0cea77d3298c67
woo0 <57060443+woo0dev@users.noreply.github.com>
2024-02-22 18:21:42 +0900

    Update ci_pre_xcodebuild.sh

--- Selterview/ci_scripts/ci_pre_xcodebuild.sh
+++ Selterview/ci_scripts/ci_pre_xcodebuild.sh
@@ -3,6 +3,8 @@
 
 echo "Environment scripts has been activated .... "
 
+output_path="/Volumes/workspace/repository/Selterview/Selterview/Resources/"
+
 echo '<?xml version="1.0" encoding="UTF-8"?>' > API_Key.plist
 echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' >> API_Key.plist
 echo '<plist version="1.0">' >> API_Key.plist
@@ -12,6 +14,8 @@ echo '    <string>$API_Key</string>' >> API_Key.plist
 echo '</dict>' >> API_Key.plist
 echo '</plist>' >> API_Key.plist
 
-echo "A.plist 생성이 완료되었습니다."
+echo "API_Key.plist 생성이 완료되었습니다."
+
+plutil -p /Volumes/workspace/repository/Selterview/Selterview/Resources/API_Key.plist
 
 exit 0
